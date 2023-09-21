07d0d32c951c44072ba2ea7ee22eaa46341c6a2e
ianmaboimonene <ian.maboi@student.moringaschool.com>
2023-09-21 21:26:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2 ;
+
+let random = Math.floor(Math.random() * 10)+ 1;
+
+let num3 = 64;
+let num4 = 60;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 5, 7, 20);
+
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
